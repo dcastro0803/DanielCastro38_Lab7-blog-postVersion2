ba0f010b4a41af610feec014b0ac8a08b3c8ae2a
dcastro0803 <37124311+dcastro0803@users.noreply.github.com>
2019-10-27 19:48:33 -0600

    Completed server

--- server.js
+++ server.js
@@ -43,7 +43,10 @@ app.get("/blog-posts", (req,res,next) => {
 
 	if(!(req.query.author)){
 
-		return res.status(200).json(post);
+		return res.status(200).json({
+			content: post,
+			message: "All posts in Database"
+		});
 	}
 
 	if(req.query.author == ""){
@@ -52,7 +55,10 @@ app.get("/blog-posts", (req,res,next) => {
 
 	post.forEach(function(postObject){
 		if(postObject.author == req.query.author){
-			return res.status(200).json(postObject);
+			return res.status(200).json({
+				content: postObject,
+				message: "Author Found"
+				});
 		}
 	});
 
@@ -60,23 +66,87 @@ app.get("/blog-posts", (req,res,next) => {
 
 });
 
-/*
-app.get("/blog-posts", (req,res,next) => {
-	//Data sent via query in postman use params
-	console.log(req.query);
-	if(!req.query.author){
-		return res.status(406).json("Missing author param");
+app.post("/blog-posts", jsonParser , (req,res,next) =>{
+	// body with all fields except id 
+	console.log(req.body);
+	if(!req.body.title || !req.body.content ||
+		!req.body.author || !req.body.publishedDate){
+		return res.status(406).json({
+			code: 406,
+			message: "title, content, author or publishedDate missed"
+		});
+	}
+	//Create object with data 
+	let postObject = {
+		id: uuid.v4(),
+		title: req.body.title,
+		content: req.body.content,
+		author: req.body.author,
+		publishedDate: req.body.publishedDate
+	}
+	// Push Object to list 
+	post.push(postObject);
+
+	//
+	return res.status(201).json({
+			code: 201,
+			content: postObject
+	});
+
+
+});
+
+app.delete("/blog-posts/:id", (req, res , next) => {
+	console.log(req.params.id);
+	console.log(post);
+	let i = 0;
+	post.forEach(function(postObject){
+		if(postObject.id == req.params.id){
+			post.splice(i,1); 
+			return res.status(200).json({
+				content: postObject,
+				message: "Object Deleted"
+			});
+		}
+		i++;
+	});
+	return res.status(404).json("ID doesn't exist");
+});
+
+app.put("/blog-posts/:id", (req, res , next) => {
+	console.log(req.params.id);
+	console.log(post);
+	if(!req.body.id){
+		return res.status(406).json("ID missed in body");
 	}
 
+	if(req.body.id != req.params.id){
+		return res.status(409).json("ID's doesn't match");
+	}
 	post.forEach(function(postObject){
-		if(postObject.author == req.query.author){
-			return res.status(200).json(postObject);
+		if(postObject.id == req.params.id){
+			//Update what the object contains
+			if(req.body.title){
+				postObject.title = req.body.title;
+			}
+			if(req.body.content){
+				postObject.content = req.body.content;
+			}
+			if(req.body.author){
+				postObject.author = req.body.author;
+			}
+			if(req.body.publishedDate){
+				postObject.publishedDate = req.body.publishedDate;
+			}
+			return res.status(202).json({
+				content: postObject,
+				message: "Object Updated"
+			});
 		}
-	});
-
-	return res.status(404).json("Author Not Found");
+	})
 });
-*/
+
+
 
 app.listen("8080", () => {
 	console.log("App running on localhost:8080"); 
